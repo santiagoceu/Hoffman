adcf8f385df41e483f137a5eb9a9954455992c9d
santiagoceu <santiago.vidal@usp.ceu.es>
2023-04-20 00:03:44 +0200

    Kinda works

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,5 +1,6 @@
 import java.security.Key;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -7,53 +8,124 @@ import java.util.stream.Stream;
  * Node object, points to a left node and a right node, may contain a character, and contains a frequency.
  */
 class Node implements Comparable<Node> {
-    char c;
+    String c;
     long frequency;
     Node left;
     Node right;
 
     @Override
     public int compareTo(Node n) {
-        return Long.compare(this.frequency, n.frequency);
+        if (this.frequency!=n.frequency)
+            return Long.compare(this.frequency, n.frequency);
+        else return CharSequence.compare(this.c,n.c);
     }
 
-    public Node(long frequency, char c) {
+    public Node(long frequency, String c) {
         this.frequency=frequency;
         this.c=c;
 
     }
     public Node(Node left, Node right) {
 
-        if (left.compareTo(right)==-1) {
+        if (left.compareTo(right) < 0) {
             this.left=left;
             this.right=right;
-        } else if (left.compareTo(right)==1) {
+        } else if (left.compareTo(right) > 0) {
             this.left=right;
             this.right=left;
-        } else {
-            int comparison = Character.compare(left.c,right.c);
-            this.left = comparison == -1 || comparison == 0 ? left : right;
-            this.right = comparison == 1 || comparison == 0 ? left : right;
         }
+//        else {
+//            int comparison = CharSequence.compare(left.c,right.c);
+//            this.left = comparison == -1 || comparison == 0 ? left : right;
+//            this.right = comparison == 1 || comparison == 0 ? left : right;
+//        }
+
+        this.frequency=left.frequency+right.frequency;
+        this.c = CharSequence.compare(left.c,right.c)==-1 ? right.c.concat(left.c): left.c.concat(right.c);
 
     }
     public String toString() {
-        return Character.toString(c);
+        return c;
     }
 }
 
 public class Huffman {
     private static PriorityQueue<Node> queue = new PriorityQueue<>();
+    private static HashMap<String,String> codes = new HashMap<String, String>();
+
     public void addNode(Node n) {
         queue.add(n);
     }
     public PriorityQueue<Node> getQueue() {
         return queue;
     }
+    public HashMap<String,String> getCodes() {return codes;}
+
+    public void calculateFrequency(String text) {
+
+        Set<Character> set = text.chars()
+                .mapToObj(ch -> (char)ch)
+                .collect(Collectors.toSet());
+        ArrayList<Character> charList=new ArrayList<>(set);
+        HashMap<Character,Long> frequencyTable = (HashMap<Character, Long>) charList.stream().collect(Collectors.groupingBy(s -> s,
+                Collectors.counting()));
+        for (Character c:frequencyTable.keySet()) {
+            Node n = new Node(frequencyTable.get(c),c.toString());
+            queue.add(n);
+        }
+    }
 
     public void connectTree() {
-        for(int i=0;i<queue.size();i++) {
 
+        while (queue.size()>1) {
+            Node a = queue.poll();
+            Node b = queue.poll();
+            Node node = new Node(a,b);
+            queue.add(node);
+            //System.out.println("A: "+a+" B: "+b);
         }
+    }
+
+    public void generateCodes(Node root,String s) {
+        connectTree();
+        if (root != null) {
+            if (root.right != null) {
+                generateCodes(root.right,s+"1");
+            }
+            if (root.left != null) {
+                generateCodes(root.left,s+"0");
+            }
+            if (root.right==null&&root.left==null) {
+                codes.put(root.c,s);
+            }
+        }
+    }
+
+    public String decode(String encoded) {
+        StringBuilder decoded = new StringBuilder();
+        connectTree();
+        Node root = getQueue().peek();
+        for (int i=0;i<encoded.length();) {
+            Node tmp = root;
+            while (tmp.right!=null&&tmp.left!=null&&i<encoded.length()) {
+                if (encoded.charAt(i)=='0') {
+                    tmp = tmp.left;
+                } else {
+                    tmp = tmp.right;
+                }
+                i++;
+            }
+            decoded.append(tmp.c);
+        }
+        return decoded.toString();
+    }
+    public String encode(String input) {
+        connectTree();
+        generateCodes(queue.peek(),"");
+        StringBuilder output = new StringBuilder();
+        for (int i = 0;i<input.length();i++) {
+            output.append(codes.get(Character.toString(input.charAt(i))));
+        }
+        return output.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String [] args) {
-        Node a = new Node(10,'a');
-        Node b = new Node(20,'b');
-        System.out.println(b.compareTo(a));
+        String text = "This is text to encode and decode.";
+//        Node a = new Node(4,"a");
+//        Node b = new Node(2,"b");
+//        Node c = new Node(2,"c");
+//        System.out.println(b.compareTo(a));
         Huffman huffman = new Huffman();
-        huffman.addNode(a);
-        huffman.addNode(b);
+//        huffman.addNode(a);
+//        huffman.addNode(b);
+//        huffman.addNode(c);
+        huffman.calculateFrequency(text);
+        String code = huffman.encode(text);
         System.out.println(huffman.getQueue().peek().toString());
+        System.out.println(huffman.getQueue().toString());
+        huffman.connectTree();
+        System.out.println(huffman.getQueue().toString());
+        System.out.println(code);
+        System.out.println(huffman.getCodes());
+        System.out.println(huffman.decode(code));
     }
 }
